b6d718628ff96e5f0229b98c3a1780ed3544f4f5
xiaotu <704323845@qq.com>
2017-04-08 14:52:41 +0800

    init project

---
+++ src/main/java/com/wechat/SpringBootWechatApplication.java
@@ -0,0 +1,12 @@
+package com.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWechatApplication.class, args);
+	}
+}
